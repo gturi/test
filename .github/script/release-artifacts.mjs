b1ec37ff3b9022e2b744d1e40f70dcd1e8142aa8
gventurini <giacomo.venturini3@gmail.com>
2021-09-19 11:57:10 +0200

    add file zipping before upload

--- .github/script/release-artifacts.mjs
+++ .github/script/release-artifacts.mjs
@@ -17,3 +17,24 @@ execSync('python -m pip install -r requirements.txt');
 const generateExecutableCmd = `pyinstaller src/main.py --onefile --name ${appName}`;
 console.log(`creating ${appName} executable in server/dist`);
 execSync(generateExecutableCmd);
+
+
+const zipName = process.argv[3] || `${appName}.zip`;
+const platform = process.platform;
+let compressionCmd;
+switch(platform){
+  case 'linux':
+    compressionCmd = `tar -zcvf server/dist/${zipName} server/dist/${appName}`
+    break;
+  case 'win32':
+    execSync('dir');
+    compressionCmd = `tar.exe -a -c -f server/dist/${zipName} server/dist/${appName}`
+    break;
+  case 'darwin':
+    compressionCmd = `zip server/dist/${zipName} server/dist/${appName}`
+    break;
+  default:
+    throw new Error(`platform ${platform} not supported`);
+}
+console.log(`creating ${zipName} in server/dist`);
+execSync(compressionCmd);
