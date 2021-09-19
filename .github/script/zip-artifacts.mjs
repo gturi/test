f23b9d8bc4d0b045399e56701442c3c63711f735
gventurini <giacomo.venturini3@gmail.com>
2021-09-19 14:04:34 +0200

    new line removal

--- .github/script/zip-artifacts.mjs
+++ .github/script/zip-artifacts.mjs
@@ -7,26 +7,30 @@ const zipName = process.argv[3] || `${appName}.zip`;
 
 const platform = process.platform;
 let compressionCmd;
+let cwd;
 let outDir;
 switch(platform) {
   case 'linux':
     console.log('LINUX ls');
     console.log(execSync('ls -laR').toString('utf8'));
-    outDir = `${execSync('pwd').toString('utf8')}/server/dist`;
-    compressionCmd = `tar -zcvf ${outDir}/${zipName} ${outDir}/${appName}`;
+    cwd = `${execSync('pwd').toString('utf8')}`.trim().replace(/(\r\n|\n|\r)/gm, "");
+    outDir = `${cwd}/server/dist`;
+    compressionCmd = `tar -zcvf ${outDir}/${cwd} ${outDir}/${appName}`;
     break;
   case 'win32':
     console.log('WIN ls');
     console.log(execSync('cd').toString('utf8'));
     console.log(execSync('cd').toString('utf8'));
-    outDir = `${execSync('cd').toString('utf8')}\\server\\dist`;
+    cwd = `${execSync('cd').toString('utf8')}`.trim().replace(/(\r\n|\n|\r)/gm, "");
+    outDir = `${cwd}\\server\\dist`;
     console.log(outDir);
     compressionCmd = `tar.exe -a -c -f ${outDir}\\${zipName} \\${appName}`;
     break;
   case 'darwin':
     console.log('MAC ls');
     console.log(execSync('ls -laR').toString('utf8'));
-    outDir = `${execSync('pwd').toString('utf8')}/server/dist`;
+    cwd = `${execSync('pwd').toString('utf8')}`.trim().replace(/(\r\n|\n|\r)/gm, "");
+    outDir = `${cwd}/server/dist`;
     compressionCmd = `zip ${outDir}/${zipName} ${outDir}/${appName}`;
     break;
   default:
