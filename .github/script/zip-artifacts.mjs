1f6e1fe7d9f0ba4691a55e148c6057fd009dae92
gventurini <giacomo.venturini3@gmail.com>
2021-09-19 16:22:48 +0200

    ignore mac directory structure when creating zip

--- .github/script/zip-artifacts.mjs
+++ .github/script/zip-artifacts.mjs
@@ -1,37 +1,29 @@
 import { execSync } from 'child_process';
-import * as path from 'path';
 
 // Default value `youtube-dl-aas` if no args provided via CLI.
 const appName = process.argv[2] || 'youtube-dl-aas';
 const zipName = process.argv[3] || `${appName}.zip`;
 
+function getCmdOutput(cmd) {
+  return execSync(cmd).toString('utf8').trim().replace(/(\r\n|\n|\r)/gm, "");
+}
+
 const platform = process.platform;
 let compressionCmd;
-let cwd;
 let outDir;
 switch(platform) {
   case 'linux':
-    console.log('LINUX ls');
     console.log(execSync('ls -laR').toString('utf8'));
-    cwd = `${execSync('pwd').toString('utf8')}`.trim().replace(/(\r\n|\n|\r)/gm, "");
-    outDir = `${cwd}/server/dist`;
+    outDir = `${getCmdOutput('pwd')}/server/dist`;
     compressionCmd = `tar -zcvf ${outDir}/${zipName} ${outDir}/${appName}`;
     break;
   case 'win32':
-    console.log('WIN ls');
-    console.log(execSync('cd').toString('utf8'));
-    console.log(execSync('cd').toString('utf8'));
-    cwd = `${execSync('cd').toString('utf8')}`.trim().replace(/(\r\n|\n|\r)/gm, "");
-    outDir = `${cwd}\\server\\dist`;
-    console.log(outDir);
+    outDir = `${getCmdOutput('cd')}\\server\\dist`;
     compressionCmd = `tar.exe -a -c -f ${outDir}\\${zipName} ${outDir}\\${appName}`;
     break;
   case 'darwin':
-    console.log('MAC ls');
-    console.log(execSync('ls -laR').toString('utf8'));
-    cwd = `${execSync('pwd').toString('utf8')}`.trim().replace(/(\r\n|\n|\r)/gm, "");
-    outDir = `${cwd}/server/dist`;
-    compressionCmd = `zip ${outDir}/${zipName} ${outDir}/${appName}`;
+    outDir = `${getCmdOutput('pwd')}/server/dist`;
+    compressionCmd = `zip -j ${outDir}/${zipName} ${outDir}/${appName}`;
     break;
   default:
     throw new Error(`platform ${platform} not supported`);
